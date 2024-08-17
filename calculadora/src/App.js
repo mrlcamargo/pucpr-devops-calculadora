40bdbf12da7eed3c5273ba82acff3fabe558973a
Murilo <murilocamargog2011@gmail.com>
2024-08-17 02:06:10 -0300

    corpo calculadora

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -17,3 +17,27 @@ const Botao = (props) => {
   )
 }
 
+function App() {
+  return(
+    <div className='painel-botoes'>
+      <Painel/>
+      <Botao tipo="botao-ac" digito="AC" />
+      <Botao tipo="botao-operacao" digito="/" />
+      <Botao tipo="botao-numero" digito="7" />
+      <Botao tipo="botao-numero" digito="8" />
+      <Botao tipo="botao-numero" digito="9" />
+      <Botao tipo="botao-operacao" digito="*" />
+      <Botao tipo="botao-numero" digito="4" />
+      <Botao tipo="botao-numero" digito="5" />
+      <Botao tipo="botao-numero" digito="6" />
+      <Botao tipo="botao-operacao" digito="-" />
+      <Botao tipo="botao-numero" digito="1" />
+      <Botao tipo="botao-numero" digito="2" />
+      <Botao tipo="botao-numero" digito="3" />
+      <Botao tipo="botao-operacao" digito="+" />
+       </div>
+    
+  );
+}
+
+export default App;
